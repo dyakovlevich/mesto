66269c7dc9a904be1f149bf832826de236ca9ddc
Dmitry Yakovlevich <dyakovlevich@yandex.ru>
2022-03-06 11:24:52 +0000

    Fix js functions names

--- js/index.js
+++ js/index.js
@@ -1,47 +1,39 @@
-let popup = document.querySelector(".popup");
-let popupContainer = document.querySelector(".popup__container");
-let profileEditButton = document.querySelector(".profile__edit-button");
-let popupCloseButton = document.querySelector(".popup__close-button");
+const popup = document.querySelector(".popup");
+const profileEditButton = document.querySelector(".profile__edit-button");
+const popupCloseButton = document.querySelector(".popup__close-button");
 
-let profileFio = document.querySelector(".profile__fio");
-let profileProfession = document.querySelector(".profile__profession");
+const profileFio = document.querySelector(".profile__fio");
+const profileProfession = document.querySelector(".profile__profession");
 
-let profileFioInput = document.querySelector(".popup__edit input[name=fio]");
-let profileProfessionInput = document.querySelector(
+const profileFioInput = document.querySelector(".popup__edit input[name=fio]");
+const profileProfessionInput = document.querySelector(
   ".popup__edit input[name=profession"
 );
 
-let profileEditForm = document.querySelector(".popup__edit");
+const profileEditForm = document.querySelector(".popup__edit");
 
-function popupOpen() {
+function openProfilePopup() {
   popup.classList.add("popup_opened");
   profileFioInput.value = profileFio.textContent;
   profileProfessionInput.value = profileProfession.textContent;
 }
 
-function popupClose() {
+function closeProfilePopup() {
   popup.classList.remove("popup_opened");
 }
 
-function formSubmitHandler(evt) {
+function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   profileFio.textContent = profileFioInput.value.trim();
   profileProfession.textContent = profileProfessionInput.value.trim();
-  popupClose();
+  closeProfilePopup();
 }
 
-profileEditButton.addEventListener("click", popupOpen);
-profileEditForm.addEventListener("submit", formSubmitHandler);
-
-//Закрытие
-popupCloseButton.addEventListener("click", popupClose);
+profileEditButton.addEventListener("click", openProfilePopup);
+profileEditForm.addEventListener("submit", handleProfileFormSubmit);
+popupCloseButton.addEventListener("click", closeProfilePopup);
 popup.addEventListener("click", function (e) {
   if (e.target === e.currentTarget) {
-    popupClose();
-  }
-});
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape") {
-    popupClose();
+    closeProfilePopup();
   }
 });
