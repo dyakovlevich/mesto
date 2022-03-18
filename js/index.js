8d544a71b5aad35c9c3c683d6acd918837b4dda5
Dmitry Yakovlevich <dyakovlevich@yandex.ru>
2022-03-18 15:49:55 +0000

    function: remove and like card

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+const cardsList = document.querySelector(".cards");
 const popup = document.querySelector(".popup");
 const profileEditButton = document.querySelector(".profile__edit-button");
 const popupCloseButton = document.querySelector(".popup__close-button");
@@ -29,6 +56,41 @@ function handleProfileFormSubmit(evt) {
   closeProfilePopup();
 }
 
+function insertCard(cardObj) {
+  const card = document
+    .querySelector(".card-template")
+    .content.firstElementChild.cloneNode(true);
+
+  card.querySelector(".card__title").textContent = cardObj.name;
+  card.querySelector(".card__img").setAttribute("src", cardObj.link);
+  card.querySelector(".card__img").setAttribute("alt", cardObj.name);
+  cardsList.append(card);
+}
+
+
+
+document.body.addEventListener("click", (event) => {
+  const card = event.target.closest(".card");
+
+  if (!card) {
+    return;
+  }
+
+  if (event.target.classList.contains("card__trash")) {
+    removeCard(card);
+  } else if (event.target.classList.contains("card__like")) {
+    likeCard(card);
+  }
+  
+  function removeCard(card) {
+    card.remove();
+  }
+  
+  function likeCard(card) {
+    card.querySelector(".card__like").classList.toggle("card__like_active");
+  }
+});
+
 profileEditButton.addEventListener("click", openProfilePopup);
 profileEditForm.addEventListener("submit", handleProfileFormSubmit);
 popupCloseButton.addEventListener("click", closeProfilePopup);
@@ -37,3 +99,5 @@ popup.addEventListener("click", function (e) {
     closeProfilePopup();
   }
 });
+
+initialCards.map(insertCard);
